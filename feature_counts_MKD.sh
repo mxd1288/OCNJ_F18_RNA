21ebb18ee9987d910a985136bda3416ee01f02b6
Melissa Drown <44902793+mxd1288@users.noreply.github.com>
2020-12-02 15:05:46 -0500

    Add files via upload
    updates fc script

--- feature_counts_MKD.sh
+++ feature_counts_MKD.sh
@@ -7,7 +7,8 @@
 #BSUB -n 8
 
 ~/software/local/subread-1.4.6-p5-Linux-x86_64/bin/featureCounts -T 8 \
--g gene_ID \
+-t gene \
+-g gene_id \
 -a /scratch/projects/fun_gen_1/MKD/GCF_011125445.2_MU-UCD_Fhet_4.1_genomic.gtf \
 -o /scratch/projects/fun_gen_1/MKD/counts/SRR74495_featurecounts \
 /scratch/projects/fun_gen_1/MKD/STAR_aligned/SRR*/*Aligned.sortedByCoord.out.bam
